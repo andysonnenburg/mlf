900078c6628a0e112d31f49003f017baf8944a3c
Andy Sonnenburg <andy22286@gmail.com>
2013-08-12 07:38:41 -0500

    removed unncessary data types

--- Loc.hs
+++ Loc.hs
@@ -1,21 +1,10 @@
-{-# LANGUAGE
-    DeriveFoldable
-  , DeriveFunctor
-  , DeriveTraversable
-  , FlexibleContexts
-  , StandaloneDeriving
-  , UndecidableInstances #-}
 module Loc
        ( Pos (..)
        , plusPos
        , Loc (..)
-       , Located (..)
-       , LocatedF (..)
        ) where
 
-import Data.Foldable
 import Data.Semigroup
-import Data.Traversable
 
 data Pos = Pos {-# UNPACK #-} !Int {-# UNPACK #-} !Int deriving (Show, Eq, Ord)
 
@@ -29,17 +18,3 @@ data Loc = Loc {-# UNPACK #-} !Pos {-# UNPACK #-} !Pos deriving Show
 
 instance Semigroup Loc where
   Loc a b <> Loc a' b' = Loc (min a a') (max b b')
-
-data Located f a = Located {-# UNPACK #-} !Loc (f (Located f a))
-
-deriving instance Show (f (Located f a)) => Show (Located f a)
-deriving instance Functor f => Functor (Located f)
-deriving instance Foldable f => Foldable (Located f)
-deriving instance Traversable f => Traversable (Located f)
-
-data LocatedF f a = LocatedF {-# UNPACK #-} !Loc (f (LocatedF f) a)
-
-deriving instance Show (f (LocatedF f) a) => Show (LocatedF f a)
-deriving instance Functor (f (LocatedF f)) => Functor (LocatedF f)
-deriving instance Foldable (f (LocatedF f)) => Foldable (LocatedF f)
-deriving instance Traversable (f (LocatedF f)) => Traversable (LocatedF f)
